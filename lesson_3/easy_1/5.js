1c06282d2151dba90a63ffe5550442a48614c630
Yu Xia <hey.xiayu@outlook.com>
2020-02-24 22:15:46 -0600

    remove lesson 3

--- lesson_3/easy_1/5.js
+++
@@ -1,6 +0,0 @@
-// What will the following code output?
-
-console.log(false == '0');
-// true, '0' will be implicitly coerced to 0, which JS evaluates to false.
-console.log(false === '0');
-// false
--- lesson_3/easy_1/9.js
+++
@@ -1,9 +0,0 @@
-// In the previous problem, our first answer added 'Dino' to the array like this:
-
-let flintstones = ["Fred", "Barney", "Wilma", "Betty", "Bambam", "Pebbles"];
-// flintstones.push("Dino");
-
-// How can we add multiple items to our array? ('Dino' and 'Hoppy')
-
-flintstones.push("Dino", "Hoppy"); // we can add multiple arguments to push
-console.log(flintstones);
