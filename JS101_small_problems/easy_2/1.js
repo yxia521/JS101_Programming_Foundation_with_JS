e99c08b385b96974e97663e2c7da53daaf74dc6a
Yu Xia <hey.xiayu@outlook.com>
2020-03-12 23:13:58 -0500

    Update easy 2

--- JS101_small_problems/easy_2/1.js
+++ JS101_small_problems/easy_2/1.js
@@ -1,9 +1,17 @@
-Welcome Stranger
-Create a function that takes 2 arguments, an array and an object. The array will contain 2 or more elements that, when combined with adjoining spaces, will produce a person's name. The object will contain two keys, "title" and "occupation", and the appropriate values. Your function should return a greeting that uses the person's full name, and mentions the person's title.
+// Welcome Stranger
+// Create a function that takes 2 arguments, an array and an object. 
+// The array will contain 2 or more elements that, when combined with adjoining spaces, 
+// will produce a person's name. The object will contain two keys, "title" and "occupation", 
+// and the appropriate values. Your function should return a greeting that uses the person's full name, 
+// and mentions the person's title.
 
-Example:
+// Example:
 
 console.log(
   greetings(["John", "Q", "Doe"], { title: "Master", occupation: "Plumber" })
 );
 // logs Hello, John Q Doe! Nice to have a Master Plumber around.
+
+function greetings(arr, obj) {
+  return `Hello, ${arr.join(' ')}! Nice to have a ${obj['title']} ${obj['occupation']} around.`;
+}
