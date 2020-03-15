a57076b5726f1fc8b3ea75fc65190049320816dd
Yu Xia <hey.xiayu@outlook.com>
2020-03-14 23:05:53 -0500

    Update easy 2

--- JS101_small_problems/easy_2/2.js
+++ JS101_small_problems/easy_2/2.js
@@ -1,9 +1,19 @@
-Greeting a user
-Write a program that will ask for user's name. The program will then greet the user. If the user writes "name!" then the computer yells back to the user.
+// Greeting a user
+// Write a program that will ask for user's name. The program will then greet the user. 
+// If the user writes "name!" then the computer yells back to the user.
 
-Examples
+// Examples
 
-What is your name? Bob
-Hello Bob.
-What is your name? Bob!
-HELLO BOB. WHY ARE WE SCREAMING?
+// What is your name? Bob
+// Hello Bob.
+// What is your name? Bob!
+// HELLO BOB. WHY ARE WE SCREAMING?
+
+const readline = require('readline-sync');
+let name = readline.question('What is your name? ');
+if (name.includes('!')) {
+  name = name.slice(0, -1);
+  console.log(`HELLO ${name.toUpperCase()}. WHY ARE WE SCREAMING?`);
+} else {
+  console.log(`Hello ${name}`);
+}
--- JS101_small_problems/easy_2/3.js
+++ JS101_small_problems/easy_2/3.js
@@ -1,6 +1,10 @@
-Multiplying Two Numbers
-Create a function that takes two arguments, multiplies them together, and returns the result.
+// Multiplying Two Numbers
+// Create a function that takes two arguments, multiplies them together, and returns the result.
 
-Example:
+// Example:
 
 console.log(multiply(5, 3) === 15); // logs true
+
+function multiply(num1, num2) {
+  return num1 * num2;
+}
