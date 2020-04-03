90746f43d115490cab8f748eac3b75ebd6141041
Yu Xia <hey.xiayu@outlook.com>
2020-04-03 16:36:02 -0500

    Finish and update easy 5

--- JS101_small_problems/easy_5/1.js
+++ JS101_small_problems/easy_5/1.js
@@ -1,12 +1,38 @@
-Cute Angles
-Write a function that takes a floating point number representing an angle between 0 and 360 degrees, and returns a string representing that angle in degrees, minutes, and seconds. You should use a degree symbol (˚) to represent degrees, a single quote (') to represent minutes, and a double quote (") to represent seconds. There are 60 minutes in a degree, and 60 seconds in a minute.
+// Cute Angles
+// Write a function that takes a floating point number representing an angle between 0 and 360 degrees, 
+// and returns a string representing that angle in degrees, minutes, and seconds. You should use a 
+// degree symbol (˚) to represent degrees, a single quote (') to represent minutes, and a double quote 
+//   (") to represent seconds. There are 60 minutes in a degree, and 60 seconds in a minute.
 
-Examples:
+// Note: your results may differ slightly depending on how you round values, 
+// but should generally be within a second or two of the results shown.
 
-dms(30);           // 30°00'00"
-dms(76.73);        // 76°43'48"
-dms(254.6);        // 254°35'59"
-dms(93.034773);    // 93°02'05"
-dms(0);            // 0°00'00"
-dms(360);          // 360°00'00" or 0°00'00"
-Note: your results may differ slightly depending on how you round values, but should generally be within a second or two of the results shown.
+var DEGREE = '\xB0'; // hexadecimal escape sequence
+var MINUTES_PER_DEGREE = 60;
+var SECONDS_PER_MINUTE = 60;
+var SECONDS_PER_DEGREE = MINUTES_PER_DEGREE * SECONDS_PER_MINUTE;
+
+// tricky part is the math
+function dms(degreesFloat) {
+  var degreesInt = Math.floor(degreesFloat);
+  var minutes = Math.floor((degreesFloat - degreesInt) * MINUTES_PER_DEGREE);
+  var seconds = Math.floor(
+    (degreesFloat - degreesInt - (minutes / MINUTES_PER_DEGREE)) *
+    SECONDS_PER_DEGREE
+  );
+
+  return String(degreesInt) + DEGREE + padZeroes(minutes) + "'" +
+                                       padZeroes(seconds) + '"';
+}
+
+function padZeroes(number) {
+  var numString = String(number);
+  return numString.length < 2 ? ('0' + numString) : numString;
+}
+
+console.log(dms(30));           // 30°00'00"
+console.log(dms(76.73));        // 76°43'48"
+console.log(dms(254.6));        // 254°35'59"
+console.log(dms(93.034773));    // 93°02'05"
+console.log(dms(0));            // 0°00'00"
+console.log(dms(360));          // 360°00'00" or 0°00'00"
---
+++ JS101_small_problems/easy_5/12.js
@@ -0,0 +1,32 @@
+// After Midnight (Part 2)
+// As seen in the previous exercise, the time of day can be represented as the 
+// number of minutes before or after midnight. If the number of minutes is positive, 
+// the time is after midnight. If the number of minutes is negative, the time is before midnight.
+
+// Write two functions that each take a time of day in 24 hour format, and return the number of 
+// minutes before and after midnight, respectively. Both functions should return a value in the range 0..1439.
+// You may not use javascript's Date class methods.
+
+const HOURS_PER_DAY = 24;
+const MINUTES_PER_HOUR = 60;
+const MINUTES_PER_DAY = HOURS_PER_DAY * MINUTES_PER_HOUR;
+
+function afterMidnight(timeStr) {
+  let [hours, minutes] = timeStr.split(":").map(num => Number(num));
+  return ((hours * MINUTES_PER_HOUR) + minutes) % MINUTES_PER_DAY;
+}
+
+function beforeMidnight(timeStr) {
+  let deltaMinutes = MINUTES_PER_DAY - afterMidnight(timeStr);
+  if (deltaMinutes === MINUTES_PER_DAY) {
+    deltaMinutes = 0;
+  }
+  return deltaMinutes;
+}
+
+console.log(afterMidnight("00:00") === 0);
+console.log(beforeMidnight("00:00") === 0);
+console.log(afterMidnight("12:34") === 754);
+console.log(beforeMidnight("12:34") === 686);
+console.log(afterMidnight("24:00") === 0);
+console.log(beforeMidnight("24:00") === 0);
